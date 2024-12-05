cbffe268a553f599010d9076c5c16ae9ea36a854
TheMaz <patriczum@gmail.com2>
2024-12-05 22:38:55 +0200

    534242

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,11 +22,17 @@
         <div class="row justify-content-center align-items-center authentication authentication-basic h-100">
             <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-6 col-sm-8 col-12">
                 <div class="my-5 d-flex justify-content-center">
+                    @php
+                        $logo = @getappconfig()->logo;
+                        if (!$logo) {
+                            $logo = 'ressources/images/logo.png';
+                        } else {
+                            $logo = asset('storage/' . $logo);
+                        }
+                    @endphp
                     <a href="{{ route('login') }}">
-                        <img src="{{ asset('assets/images/brand-logos/desktop-logo.png') }}" alt="logo"
-                            class="desktop-logo">
-                        <img src="{{ asset('assets/images/brand-logos/desktop-dark.png') }}" alt="logo"
-                            class="desktop-dark">
+                        <img src="{{ $logo }}" alt="logo" class="desktop-logo">
+                        <img src="{{ $logo }}" alt="logo" class="desktop-dark">
                     </a>
                 </div>
                 <div class="card custom-card">
